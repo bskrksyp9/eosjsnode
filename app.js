214143fc7620659ddf48ea12cff965e399a2cff0
bskrksyp9 <bskrksyp9@zoho.com>
2021-08-24 19:25:22 +0530

    cors fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,6 +14,7 @@ var balance = require('./routes/newaccount');
 
 var app = express();
 
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
